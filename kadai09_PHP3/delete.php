5972b164f2e9de1531a35ee9418dda2af174a980
chinao624 <160087811+chinao624@users.noreply.github.com>
2024-04-13 12:46:17 +0900

    local提出

---
+++ kadai09_PHP3/delete.php
@@ -0,0 +1,27 @@
+<?php
+ini_set('display_errors', '1');
+error_reporting(E_ALL);
+
+// 1. DB接続
+include("funcs.php");
+$pdo = db_conn();
+
+
+// 2. 削除対象の名前を取得
+$id = $_GET['id'];
+
+// 3. データを削除
+$stmt = $pdo->prepare("DELETE FROM profile_builder WHERE id = :id");
+$stmt->bindValue(':id', $id, PDO::PARAM_INT);
+$status = $stmt->execute(); //実行
+
+if ($status==false) {
+    sql_error($stmt);
+    echo "Dataの削除に失敗しました。エラー: " . $error[2];
+    
+} else {
+    redirect("select.php");
+    echo "Dataを削除しました。";
+}
+
+?>
